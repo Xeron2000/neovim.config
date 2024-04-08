c53e0563aece209f954e1429e15d235e3901a055
Xeron <cw48565@gmail.com>
2024-04-08 22:39:42 +0800

    2024.4.8

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,16 @@
+return {
+{
+  "nvim-cmp",
+  dependencies = {
+    "saadparwaiz1/cmp_luasnip",
+  },
+  opts = function(_, opts)
+    opts.snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    }
+    table.insert(opts.sources, { name = "luasnip" })
+  end,
+}
+}
---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    opts = {
+      config = {
+        header = {},
+        week_header = {
+          enable = true,
+        },
+      },
+    },
+  },
+}

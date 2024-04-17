02833482f3494cf5b93fa368b77709644658f717
Xeron <111@gmail.com>
2024-04-17 21:08:27 +0800

    2024.4.17

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      integrations = {
+        telescope = true,
+        harpoon = true,
+        mason = true,
+        neotest = true,
+      }
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}

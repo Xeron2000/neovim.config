264193f6e644648e04001bafd7de9fbf2b722421
Xeron <cw48565@gmail.com>
2024-11-24 16:50:45 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.opt.clipboard = "unnamedplus"
+-- 禁用删除时自动复制到剪贴板
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true })
+vim.api.nvim_set_keymap("n", "D", '"_D', { noremap = true })
+vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true })
